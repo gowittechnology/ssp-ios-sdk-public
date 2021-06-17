b01433c2f243843fec6b9f339c6a70e0a5c785a5
Dogus Yigit Ozcelik <dogusyigitozcelik@gmail.com>
2021-06-17 04:07:55 +0300

    build settings

---
+++ build/sspadkit.framework-iphoneos.xcarchive/Products/Library/Frameworks/sspadkit.framework/Headers/sspadkit.h
@@ -0,0 +1,18 @@
+//
+//  sspadkit.h
+//  sspadkit
+//
+//  Created by Dogus Yigit Ozcelik on 26.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sspadkit.
+FOUNDATION_EXPORT double sspadkitVersionNumber;
+
+//! Project version string for sspadkit.
+FOUNDATION_EXPORT const unsigned char sspadkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sspadkit/PublicHeader.h>
+
+
